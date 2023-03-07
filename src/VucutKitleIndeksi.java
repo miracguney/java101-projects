7e912f8cc2c4cb0645650e32b585b3070d86b1bb
miracguney <miracgguneyy@gmail.com>
2023-03-07 03:08:34 +0300

    VucutKitleIndeksi.java eklendi

---
+++ src/VucutKitleIndeksi.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class VucutKitleIndeksi {
+    public static void main(String[] args){
+
+        double boy, kilo, vki;
+
+        Scanner in = new Scanner (System.in);
+
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz : ");
+        boy = in.nextInt();
+        System.out.print("Lütfen kilonuzu giriniz : ");
+        kilo = in.nextDouble();
+
+        vki = kilo / (boy * boy);
+
+        System.out.println("Vücut Kitle İndeksiniz :" + vki);
+
+
+    }
+}
